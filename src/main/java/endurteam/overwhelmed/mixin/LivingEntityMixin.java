c713264771dd013360fb0da7b02887ebd8f5cdc1
cheesenull-butter <89346542+cheesenull-butter@users.noreply.github.com>
2024-05-22 20:19:27 +0700

    Can't fix it, no wonder why I left

--- src/main/java/endurteam/overwhelmed/mixin/LivingEntityMixin.java
+++ src/main/java/endurteam/overwhelmed/mixin/LivingEntityMixin.java
@@ -4,6 +4,7 @@ import endurteam.overwhelmed.block.OverwhelmedBlocks;
 import net.minecraft.block.BlockState;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.EntityType;
+import net.minecraft.entity.LivingEntity;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.Direction;
 import net.minecraft.world.World;
@@ -15,23 +16,23 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 import java.util.Optional;
 
-@Mixin(Entity.class)
+@Mixin(LivingEntity.class)
 public abstract class LivingEntityMixin extends Entity {
 
 	@Shadow
-	private Optional<BlockPos> lastClimbablePos;
+	private Optional<BlockPos> climbingPos;
 
-	public LivingEntityMixin(EntityType<?> type, World world) {
-		super(type, world);
-	}
+    protected LivingEntityMixin(EntityType<?> entityType, World world) {
+        super(entityType, world);
+    }
 
-	@Inject(method = "isClimibing", at = @At("TAIL"), cancellable = true)
+    @Inject(method = "isClimbing", at = @At("TAIL"), cancellable = true)
 	private void onClimableMixin(CallbackInfoReturnable<Boolean> cir) {
 		Direction result = Direction.fromRotation(this.getY());
 		BlockPos blockPos = this.getBlockPos().offset(result);
 		BlockState state = this.getWorld().getBlockState(blockPos);
 		if (state.isOf(OverwhelmedBlocks.GOO_BLOCK)) {
-			this.lastClimbablePos = Optional.of(blockPos);
+			this.climbingPos = Optional.of(blockPos);
 			cir.setReturnValue(true);
 		}
 	}
