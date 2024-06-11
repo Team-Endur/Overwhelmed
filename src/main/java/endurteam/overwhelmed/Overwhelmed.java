583626373c475edcc21b27a0c18e4b96478f14fd
cheesenull-butter <89346542+cheesenull-butter@users.noreply.github.com>
2024-06-11 15:48:02 +0700

    No gen

--- src/main/java/endurteam/overwhelmed/Overwhelmed.java
+++ src/main/java/endurteam/overwhelmed/Overwhelmed.java
@@ -6,14 +6,27 @@ import endurteam.overwhelmed.entity.OverwhelmedEntities;
 import endurteam.overwhelmed.item.OverwhelmedCreativeTabs;
 import endurteam.overwhelmed.item.OverwhelmedItems;
 import endurteam.overwhelmed.sound.OverwhelmedSounds;
+import endurteam.overwhelmed.worldgen.OverwhelmedBiomeTags;
 import net.fabricmc.api.ModInitializer;
 
+import net.fabricmc.fabric.api.biome.v1.BiomeModifications;
+import net.fabricmc.fabric.api.biome.v1.BiomeSelectors;
 import net.fabricmc.fabric.api.object.builder.v1.entity.FabricDefaultAttributeRegistry;
 import net.fabricmc.fabric.api.registry.FlammableBlockRegistry;
 import net.fabricmc.fabric.api.registry.StrippableBlockRegistry;
+import net.minecraft.registry.RegistryKey;
+import net.minecraft.registry.RegistryKeys;
+import net.minecraft.registry.tag.BiomeTags;
+import net.minecraft.util.Identifier;
+import net.minecraft.world.biome.Biome;
+import net.minecraft.world.gen.GenerationStep;
+import net.minecraft.world.gen.feature.PlacedFeature;
 
 public class Overwhelmed implements ModInitializer {
 	public static final String MOD_ID = "overwhelmed";
+
+	public static final RegistryKey<PlacedFeature> ALTAIR_ORE_PLACED_KEY = RegistryKey.of(RegistryKeys.PLACED_FEATURE,
+			new Identifier("overwhelmed","ore_altair"));
 
 	@Override
 	public void onInitialize() {
@@ -32,6 +45,9 @@ public class Overwhelmed implements ModInitializer {
 		FlammableBlockRegistry.getDefaultInstance().add(OverwhelmedBlocks.WILLOW_LEAVES, 30, 60);
 
 		FabricDefaultAttributeRegistry.register(OverwhelmedEntities.HORNET, HornetEntity.createHornetAttributes());
+
+		BiomeModifications.addFeature(BiomeSelectors.tag(OverwhelmedBiomeTags.ORE_ALTAIR),
+				GenerationStep.Feature.UNDERGROUND_ORES, ALTAIR_ORE_PLACED_KEY);
 
 	}
 }
---
+++ src/main/java/endurteam/overwhelmed/worldgen/OverwhelmedBiomeTags.java
@@ -0,0 +1,12 @@
+package endurteam.overwhelmed.worldgen;
+
+import net.minecraft.registry.RegistryKeys;
+import net.minecraft.registry.tag.TagKey;
+import net.minecraft.util.Identifier;
+import net.minecraft.world.biome.Biome;
+
+public class OverwhelmedBiomeTags {
+
+    public static final TagKey<Biome> ORE_ALTAIR = TagKey.of(RegistryKeys.BIOME, new Identifier("ore_altair"));
+
+}
