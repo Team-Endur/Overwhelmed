bcc2a88dc3e70643fb643bcde43d39f5afdd060a
zerocool <46464404+z3roco01@users.noreply.github.com>
2024-06-26 23:21:50 -0500

    make clot block not spawn ontop of leaves

--- src/main/java/endurteam/overwhelmed/block/ClotBlock.java
+++ src/main/java/endurteam/overwhelmed/block/ClotBlock.java
@@ -6,6 +6,7 @@ import net.minecraft.block.BlockState;
 import net.minecraft.block.FallingBlock;
 import net.minecraft.block.ShapeContext;
 import net.minecraft.fluid.Fluids;
+import net.minecraft.registry.tag.BlockTags;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.Vec3d;
 import net.minecraft.util.shape.VoxelShape;
@@ -35,7 +36,7 @@ public class ClotBlock extends FallingBlock {
     protected boolean canPlaceAt(BlockState state, WorldView world, BlockPos pos) {
         BlockState blockBelow = world.getBlockState(pos.down());
         // if fluids state of below block is empty, meaning its not a fluid, then can place, else cannot
-        return blockBelow.getFluidState() == Fluids.EMPTY.getDefaultState();
+        return blockBelow.getFluidState() == Fluids.EMPTY.getDefaultState() && blockBelow.isSolid() && !blockBelow.isIn(BlockTags.LEAVES);
     }
 
     @Override
